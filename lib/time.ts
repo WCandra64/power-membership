6413b71180876cd79774dcb088f643bffee34b48
WCandra64 <wcandra01@gmail.com>
2026-06-29 12:20:58 +0700

    announcement debug 4

--- lib/time.ts
+++ lib/time.ts
@@ -14,7 +14,11 @@ export function storeTime(time = localTime()) {
 }
 
 export function storeDate(date = localTime()) {
-  return date.toISOString().split(" ")[0];
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+
+  return `${year}-${month}-${day}`;
 }
 
 // SAFE:
