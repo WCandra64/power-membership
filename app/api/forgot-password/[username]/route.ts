7eeab92cd83f6a6c9557e7128251ec610cad847e
WCandra64 <wcandra01@gmail.com>
2026-07-16 23:43:44 +0700

    reset password

---
+++ app/api/forgot-password/[username]/route.ts
@@ -0,0 +1,163 @@
+import { db } from "@/lib/db";
+import { storeDate, storeTime } from "@/lib/time";
+import bcrypt from "bcrypt";
+
+type Props = {
+  params: Promise<{ username: string }>;
+};
+
+export async function GET(req: Request, { params }: Props) {
+  try {
+    const { username } = await params;
+
+    if (!username?.trim()) {
+      return Response.json(
+        { message: "Username wajib diisi." },
+        { status: 400 }
+      );
+    }
+
+    const [rows] = await db.query(`
+      SELECT u.username
+      FROM users u
+      WHERE u.username = ? AND u.role = 'member'
+      LIMIT 1
+      `, [username.trim()]
+    );
+
+    const users = rows as { username: string }[];
+
+    if (users.length === 0) {
+      return Response.json(
+        {
+          success: false,
+          message: "Username tidak ditemukan.",
+        },
+        { status: 404 }
+      );
+    }
+
+    return Response.json({ success: true, message: "Username ditemukan.", });
+
+  } catch (err: any) {
+    console.error("CHECK USERNAME ERROR:", err);
+
+    return Response.json(
+      { success: false, message: "Server error.", error: err?.message || err, },
+      { status: 500 }
+    );
+  }
+}
+
+export async function POST(req: Request, { params } : Props) {
+  try {
+    const { username } = await params;
+
+    const { registrationDate, registrationTime, } = await req.json();
+
+    if ( !username || !registrationDate || !registrationTime) {
+      return Response.json(
+        { message: "Semua data wajib diisi." },
+        { status: 400 }
+      );
+    }
+
+    const [rows] = await db.query(`
+      SELECT created_at FROM users
+      WHERE username = ? AND role = 'member'
+      LIMIT 1
+      `, [username]
+    );
+
+    const users = rows as { created_at: Date }[];
+
+    if (!users.length) {
+      return Response.json(
+        { message: "Username tidak ditemukan." },
+        { status: 404 }
+      );
+    }
+
+    const createdAt = new Date(users[0].created_at);
+
+    // Date comparison
+    const dbDate = storeDate(createdAt);
+
+    // Hour:Minute comparison
+    const dbTime =
+      `${String(createdAt.getHours()).padStart(2, "0")}:${String(createdAt.getMinutes()).padStart(2, "0")}`;
+
+    if (dbDate !== registrationDate ||dbTime !== registrationTime) {
+      return Response.json(
+        { success: false, message: "Tanggal atau waktu registrasi tidak sesuai." },
+        { status: 400 }
+      );
+    }
+
+    return Response.json({ success: true, message: "Verifikasi berhasil." });
+
+  } catch (err: any) {
+    console.error("VERIFY REGISTRATION TIME ERROR:", err);
+
+    return Response.json(
+      { message: "Server error.", error: err?.message || err, },
+      { status: 500 }
+    );
+  }
+}
+
+export async function PATCH(req: Request, { params }: Props) {
+  try {
+    const { username } = await params;
+    const { password } = await req.json();
+
+    if (!username || !password) {
+      return Response.json(
+        { message: "Username dan password wajib diisi." },
+        { status: 400 }
+      );
+    }
+
+    // if (password.length < 6) {
+    //   return Response.json(
+    //     { message: "Password minimal 6 karakter." },
+    //     { status: 400 }
+    //   );
+    // }
+
+    const [rows] = await db.query(`
+      SELECT id FROM users
+      WHERE username = ? AND role = 'member'
+      LIMIT 1
+      `, [username]
+    );
+
+    const users = rows as { id: number }[];
+
+    if (users.length === 0) {
+      return Response.json(
+        { message: "Username tidak ditemukan." },
+        { status: 404 }
+      );
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await db.query(`
+      UPDATE users
+      SET password = ?, updated_at = ?
+      WHERE id = ?
+      `, [hashedPassword, storeTime(), users[0].id]
+    );
+
+    return Response.json({ success: true, message: "Password berhasil diperbarui.", });
+
+  } catch (err: any) {
+    console.error("UPDATE PASSWORD ERROR:", err);
+
+    return Response.json(
+      { message: "Server error.", error: err?.message || err, },
+      { status: 500 }
+    );
+  }
+}
--- components/ClientLogin.tsx
+++ components/ClientLogin.tsx
@@ -5,6 +5,7 @@ import { useActionState, useEffect, useState } from "react";
 import { loginAction } from "@/app/actions/auth";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import BareButton from "./BareButton";
 
 const initialState = {
   error: "",
@@ -24,7 +25,21 @@ export default function LoginPage() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
+  const [confirmPassword, setConfirmPassword] = useState("");
+
   const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+  const [forgotPassword, setForgotPassword] = useState(false);
+  const [fpwStep, setFpwStep] = useState(1);
+  const [registrationDate, setRegistrationDate] = useState("");
+  const [registrationTime, setRegistrationTime] = useState("");
+  const [checkUsername, setCheckUsername] = useState(false);
+  const [checkRegisDate, setCheckRegisDate] = useState(false);
+  const [checkRegisTime, setCheckRegisTime] = useState(false);
+  const [checkPassword, setCheckPassword] = useState(false);
+  const [fpwLoading, setFpwLoading] = useState(false);
+  const [successfulChange, setSuccessfulChange] = useState(false);
 
   const [loginDisabled, setLoginDisabled] = useState(false);
 
@@ -32,6 +47,95 @@ export default function LoginPage() {
     username: false,
     password: false,
   });
+
+  function resetForgotPassword() {
+    setForgotPassword(false);
+    setUsername("");
+    setPassword("");
+    setConfirmPassword("");
+    setRegistrationDate("");
+    setRegistrationTime("");
+    setFpwStep(1);
+    setCheckUsername(false);
+    setCheckRegisDate(false);
+    setCheckRegisTime(false);
+    setCheckPassword(false);
+    setFpwLoading(false);
+  }
+
+  async function findUsername() {
+    setFpwLoading(true);
+
+    const res = await fetch("/api/forgot-password/"+username, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await res.json();
+
+    if (res.ok) {
+      setFpwStep(2);
+    } else {
+      setCheckUsername(true);
+    }
+
+    setFpwLoading(false);
+  }
+
+  async function verifyRegistrationDate() {
+    setFpwLoading(true);
+
+    const res = await fetch("/api/forgot-password/"+username, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        registrationDate,
+        registrationTime
+      }),
+    });
+
+    const data = await res.json();
+
+    if (res.ok) {
+      setFpwStep(3);
+    } else {
+      setCheckRegisDate(true);
+      setCheckRegisTime(true);
+    }
+
+    setFpwLoading(false);
+  }
+  
+  async function updatePassword() {
+    setFpwLoading(true);
+
+    if (password === confirmPassword) {
+      const res = await fetch("/api/forgot-password/"+username, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          password,
+        }),
+      });
+
+      const data = await res.json();
+
+      if (res.ok) {
+        setForgotPassword(false);
+        resetForgotPassword();
+        setSuccessfulChange(true);
+      }
+    } else
+      setCheckPassword(true);
+
+    setFpwLoading(false);
+  }
 
   // const toast = () => {
   //   setShowToast(true);
@@ -99,6 +203,19 @@ export default function LoginPage() {
       return () => clearTimeout(timer);
     };
   }, [state?.ts])
+
+  useEffect(() => {
+    if(successfulChange) {
+      setShowToast(true);
+
+      const timer = setTimeout(() => {
+        setShowToast(false);
+        setSuccessfulChange(false);
+      }, 3000);
+      
+      return () => clearTimeout(timer);
+    };
+  }, [successfulChange])
   
   useEffect(() => {
     if(!interacted) return;
@@ -109,6 +226,19 @@ export default function LoginPage() {
   useEffect(() => {
     setLoginDisabled(pending || !username || !password ? true : false);
   }, [pending, password, username])
+
+  useEffect(() => {
+    if(forgotPassword)
+      setCheckUsername(false);
+      setCheckRegisDate(false);
+      setCheckRegisTime(false);
+      setCheckPassword(false);
+  }, [username, registrationDate, registrationTime, password, confirmPassword])
+
+  useEffect(() => {
+    if(registrationDate !== "" && registrationTime !== "")
+      verifyRegistrationDate();
+  }, [registrationDate, registrationTime])
 
   return (
     <main className="w-full h-[calc(100dvh-theme(spacing.12))] px-6 pb-4">
@@ -146,10 +276,10 @@ export default function LoginPage() {
                   className={`
                     w-full rounded-sm px-4 py-3 text-sm
                     border-1 border-stroke/40 focus:outline-2 outline-stroke
-                    ${username ? "outline-2 bg-background" : "bg-paragraph/5"}
+                    ${!forgotPassword && username ? "outline-2 bg-background" : "bg-paragraph/5"}
                     ${errors.username && "bg-prime/20"}
                   `}
-                  value={username}
+                  value={forgotPassword ? "" : username}
                   onChange={(e) => {
                     setUsername(e.target.value);
                     setInteracted(true);
@@ -167,7 +297,7 @@ export default function LoginPage() {
                   Password
                   <span className={`text-prime ${!errors.password && "hidden"}`}>{" (Pasword harus diisi!)"}</span>
                 </label>
-                <div className={`flex items-center gap-2 w-full rounded-sm px-4 py-3 text-md border-1 border-stroke/40 outline-stroke focus-within:outline-2 ${password ? "outline-2 bg-background" : "bg-paragraph/5"} ${errors.password && "bg-prime/20"}`}>
+                <div className={`flex items-center gap-2 w-full rounded-sm px-4 py-3 text-md border-1 border-stroke/40 outline-stroke focus-within:outline-2 ${!forgotPassword && password ? "outline-2 bg-background" : "bg-paragraph/5"} ${errors.password && "bg-prime/20"}`}>
 
                   <input
                     type={showPassword ? "text" : "password"}
@@ -175,7 +305,7 @@ export default function LoginPage() {
                     placeholder="Password"
                     autoComplete="current-password"
                     className="w-full outline-none focus:outline-none focus:ring-0"
-                    value={password}
+                    value={forgotPassword ? "" : password}
                     onChange={(e) => {
                       setPassword(e.target.value);
                       setInteracted(true);
@@ -185,6 +315,8 @@ export default function LoginPage() {
                   <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} onClick={() => setShowPassword(!showPassword)} size="lg" className="w-4 text-stroke hover:text-stroke/40 cursor-pointer" />
                 </div>
               </div>
+
+              <div onClick={() => {setForgotPassword(true); setUsername("");}} className="underline text-prime text-sm cursor-pointer hover:text-stroke">Lupa Password?</div>
 
             </div>
 
@@ -199,10 +331,137 @@ export default function LoginPage() {
           </form>
 
           {showToast && (
-            <div className="fixed top-16 left-1/2 -translate-x-1/2 bg-rose-900 text-white px-6 py-4 rounded-md shadow-xl text-background text-center z-50">
-              {state?.error}
+            <div className={`fixed top-16 left-1/2 -translate-x-1/2 ${successfulChange? "bg-green-600" : "bg-rose-900"} text-white px-6 py-4 rounded-md shadow-xl text-background text-center z-50`}>
+              {successfulChange? "Password berhasil diganti." : state?.error}
               {/* {state?.error ? state.error : (errors.username || errors.password) ? "Username dan password harus diisi!" : errors.validation} */}
               {/* {errors.username ? errors.username : errors.password ? errors.password : errors.validation} */}
+            </div>
+          )}
+
+          {forgotPassword && (
+            <div className={`fixed inset-0 flex items-center justify-center z-50 w-full h-full ${!forgotPassword && "hidden"}`}>
+            
+              <div onClick={() => resetForgotPassword()} className="absolute inset-0 bg-stroke/80" />
+    
+              <div className="absolute w-full max-w-md p-6">
+                <div className="flex flex-col gap-4 bg-background p-6 rounded-sm">
+                  <h3 className="text-lg font-black font-mulish text-center">Ganti Password</h3>
+    
+                  <hr className="border-stroke" />
+
+                  {fpwStep === 1 && (
+                    <>
+                      <span className="text-sm">Masukkan username anda</span>
+                      <div className="flex flex-col">
+                        <label className="text-xs">
+                          Username
+                          <span className="text-prime">{checkUsername && " (Username tidak ditemukan)"}</span>
+                        </label>
+                        <input
+                          type="text"
+                          name="findUsername"
+                          placeholder="Username anda"
+                          value={username}
+                          onChange={(e) => setUsername(e.target.value)}
+                          disabled={fpwStep > 1}
+                          className={`rounded-lg border px-3 py-2 ${checkUsername && "bg-prime/20"}`}
+                        />
+                      </div>
+                    </>
+                  )}
+
+                  {fpwStep === 2 && (
+                    <>
+                      <span className="text-sm">Masukkan waktu pendaftaran (silakan tanya admin)</span>
+                      <div className="flex flex-col">
+                        <label className="text-xs">
+                          Tanggal Pendaftaran
+                          <span className="text-prime">{checkRegisDate && " (Tanggal salah)"}</span>
+                        </label>
+                        <input
+                          type="date"
+                          name="regisDate"
+                          value={registrationDate}
+                          onChange={(e) => setRegistrationDate(e.target.value)}
+                          disabled={fpwStep > 2}
+                          className={`rounded-lg border px-3 py-2 ${checkRegisDate && "bg-prime/20"}`}
+                        />
+                      </div>
+                      <div className="flex flex-col">
+                        <label className="text-xs">
+                          Jam Pendaftaran
+                          <span className="text-prime">{checkRegisTime && " (Jam salah)"}</span>
+                        </label>
+                        <input
+                          type="time"
+                          name="regisTime"
+                          value={registrationTime}
+                          onChange={(e) => setRegistrationTime(e.target.value)}
+                          disabled={fpwStep > 2}
+                          className={`rounded-lg border px-3 py-2 ${checkRegisTime && "bg-prime/20"}`}
+                        />
+                      </div>
+                    </>
+                  )}
+
+                  {fpwStep === 3 && (
+                    <>
+                      <span className="text-sm">Masukkan password baru</span>
+                      <div>
+                        <label className="text-xs font-medium text-stroke">
+                          Password
+                          <span className="text-prime">{checkPassword && " (Password tidak sama)"}</span>
+                        </label>
+                        <div className={`flex items-center gap-2 w-full rounded-lg px-4 py-3 border focus-within:outline-2 ${checkPassword && "bg-prime/20"}`}>
+                          <input
+                            type={showPassword ? "text" : "password"}
+                            name="password"
+                            placeholder="Password baru"
+                            autoComplete="current-password"
+                            className="w-full outline-none focus:outline-none focus:ring-0"
+                            value={password}
+                            onChange={(e) => {
+                              setPassword(e.target.value);
+                            }}
+                          />
+                          <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} onClick={() => setShowPassword(!showPassword)} size="lg" className="w-4 text-stroke hover:text-stroke/40 cursor-pointer" />
+                        </div>
+                      </div>
+                      <div>
+                        <label className="text-xs font-medium text-stroke">
+                          Konfirmasi Password
+                          <span className="text-prime">{checkPassword && " (Password tidak sama)"}</span>
+                        </label>
+                        <div className={`flex items-center gap-2 w-full rounded-lg px-4 py-3 border focus-within:outline-2 ${checkPassword && "bg-prime/20"}`}>
+                          <input
+                            type={showConfirmPassword ? "text" : "password"}
+                            name="password"
+                            placeholder="Password baru (ulang)"
+                            autoComplete="current-password"
+                            className="w-full outline-none focus:outline-none focus:ring-0"
+                            value={confirmPassword}
+                            onChange={(e) => {
+                              setConfirmPassword(e.target.value);
+                            }}
+                          />
+                          <FontAwesomeIcon icon={showConfirmPassword ? faEyeSlash : faEye} onClick={() => setShowConfirmPassword(!showConfirmPassword)} size="lg" className="w-4 text-stroke hover:text-stroke/40 cursor-pointer" />
+                        </div>
+                      </div>
+                    </>
+                  )}
+    
+                  <div className="flex flex-col gap-4 pt-6">
+                    {fpwStep === 1 ? (
+                      <PrimaryButton onClick={() => findUsername()} disabled={!username || checkUsername || fpwLoading}>
+                        {fpwLoading ? "Loading..." : "Cari"}
+                      </PrimaryButton>
+                    ) :
+                      <PrimaryButton onClick={() => updatePassword()} disabled={fpwStep === 2 || !password || !confirmPassword || checkPassword || fpwLoading}>{fpwLoading ? "Loading..." : "Ganti"}</PrimaryButton>
+                    }
+                    <BareButton onClick={() => resetForgotPassword()} disabled={fpwLoading}>Batal</BareButton>
+                  </div>
+                </div>
+              </div>
             </div>
           )}
         </>
